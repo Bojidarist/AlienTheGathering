4b00aa1f8ebb3b148e6504ea910e7284134f4903
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2020-08-08 21:40:28 +0300

    Add SpaceshipController

---
+++ ATG/AlienTheGathering/Assets/Scripts/Controllers/SpaceshipController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace ATG.Controllers
+{
+    public class SpaceshipController : MonoBehaviour
+    {
+        [SerializeField] private float speed = 1.0f;
+        private float horizontalInput = 0.0f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            horizontalInput = Input.GetAxisRaw("Horizontal");
+        }
+
+        private void FixedUpdate()
+        {
+            gameObject.transform.Translate((horizontalInput * speed) * Time.fixedDeltaTime, 0.0f, 0.0f);
+        }
+    }
+}
