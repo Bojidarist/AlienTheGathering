b46a3609e3e1a3a2b090814fd21253db129c4b3c
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2020-08-08 20:38:19 +0300

    Add Game/Audio Manager

---
+++ ATG/AlienTheGathering/Assets/Scripts/Core/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace ATG.Core
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager Instance { get; set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+    }
+}
---
+++ ATG/AlienTheGathering/Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace ATG.Core
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance { get; set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+    }
+}
