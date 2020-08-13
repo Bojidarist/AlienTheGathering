0e8a0bb04849f7bacd2446687a8143707905bd25
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2020-08-13 18:28:59 +0300

    Add ground prefabs & start level dev

--- ATG/AlienTheGathering/Assets/Scripts/Controllers/CowController.cs
+++ ATG/AlienTheGathering/Assets/Scripts/Controllers/CowController.cs
@@ -4,39 +4,31 @@ namespace ATG.Controllers
 {
     public class CowController : MonoBehaviour
     {
-        [SerializeField] private GameObject ground = null;
-        [SerializeField] private float speed = 1.0f;
+        [SerializeField] private float speed = 3.0f;
         [SerializeField] private float turnbackMargin = 1.0f;
         private int direction = 0;
         private Vector3 movementVec = Vector3.zero;
-        private float groundStartX = 0.0f;
-        private float groundEndX = 0.0f;
-        private RectTransform groundRect = default;
+        private ScreenBorderDetector borderDetector = default;
 
         void Start()
         {
-            groundRect = ground.GetComponent<RectTransform>();
-            direction = new int[2] { -1, 1 }[Random.Range(0,2)];    // Pick random direction
+            direction = new int[2] { -1, 1 }[Random.Range(0, 2)];    // Pick random direction
+            borderDetector = FindObjectOfType<ScreenBorderDetector>();
         }
 
         void Update()
         {
-            if(ground != null)
+            if (transform.position.x >= borderDetector.rightBorder)
             {
-                groundStartX = ground.transform.position.x - ground.transform.localScale.x + turnbackMargin;
-                groundEndX = ground.transform.position.x + ground.transform.localScale.x - turnbackMargin;
-
-                if (transform.position.x >= groundEndX)
-                {
-                    direction = -1;
-                }
-                else if(transform.position.x <= groundStartX){
-                    direction = 1;
-                }
-
-                movementVec.x = (direction * speed) * Time.deltaTime;
-                transform.position += movementVec;
+                direction = -1;
             }
+            else if (transform.position.x <= borderDetector.leftBorder)
+            {
+                direction = 1;
+            }
+
+            movementVec.x = (direction * speed) * Time.deltaTime;
+            transform.position += movementVec;
         }
     }
 }
